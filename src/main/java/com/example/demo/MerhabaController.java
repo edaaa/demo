a0f6d79daf1256e3df22bcba797e6bf0a9d4be7f
edaaa <eesmekaya17@outlook.com>
2017-10-25 01:38:48 +0300

    dene

--- src/main/java/com/example/demo/MerhabaController.java
+++ src/main/java/com/example/demo/MerhabaController.java
@@ -6,15 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MerhabaController {
-  /*  private static final String PATH = "/merhaba";
-    @RequestMapping(value = PATH)
+
+    @RequestMapping("/merhaba")
     public void merhaba(Model model) {
         String mesajicerigi="Merhaba Dünya";
         model.addAttribute("mesaj", mesajicerigi);
-    } */
-    @RequestMapping("/merhaba")
-    String merhaba() {
-        return "merhaba";
     }
-
 }
