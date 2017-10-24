f50e46039260a2fac195423699da6f0eed47f7e9
edaaa <eesmekaya17@outlook.com>
2017-10-25 02:12:52 +0300

    dene

--- src/main/java/com/example/demo/MerhabaController.java
+++ src/main/java/com/example/demo/MerhabaController.java
@@ -4,12 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class MerhabaController {
 
-    @RequestMapping("/merhaba")
+   /* @RequestMapping("/merhaba")
     public void merhaba(Model model) {
         String mesajicerigi="Merhaba Dünya";
-        model.addAttribute("mesaj", mesajicerigi);
+        model.addAttribute("mesaj", mesajicerigi);*/
+    //}
+    @RequestMapping("/")
+    public String home(Map<String, Object> model) {
+        model.put("message", "HowToDoInJava Reader !!");
+        return "index";
+    }
+    @RequestMapping(value={"/","merhaba"})
+    public String home(){
+        return "merhaba";
+    }
+
+    @RequestMapping("/next")
+    public String next(Map<String, Object> model) {
+        model.put("message", "You are in new page !!");
+        return "next";
     }
 }
